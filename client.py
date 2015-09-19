5ea2d793a00ebf98994afc367911058daa73bc46
Jean-David Moisan <vynecompiler@gmail.com>
2015-09-18 19:34:06 -0700

    Result after today's stream and more

--- client.py
+++ client.py
@@ -4,6 +4,8 @@ import sys
 import socket
 import struct
 import threading
+import time
+import math
 
 import proto.messages_robocup_ssl_wrapper_pb2 as wrapper
 from proto.grSim_Packet_pb2 import grSim_Packet
@@ -19,6 +21,11 @@ from PyQt4.QtNetwork import *
 MCAST_GRP = '224.5.23.2'
 MCAST_PORT = 10020
 
+SEND_ADDR = '127.0.0.1'
+SEND_PORT = 20011
+
+udpsocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.bind(('', MCAST_PORT))  # use MCAST_GRP instead of '' to listen only
@@ -27,6 +34,8 @@ mreq = struct.pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
 
 sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
+shoot = False
+fakeOri = 0
 playAll = True
 
 def debugP(text):
@@ -62,8 +71,12 @@ class RecvVision(threading.Thread):
             debugP ("Time Sent: {}".format(wp.detection.t_sent))
             debugP ("Camera ID: {}".format(wp.detection.camera_id))
 
-            wc.teams.append(wp.detection.robots_yellow)
-            wc.teams.append(wp.detection.robots_blue)
+            if len(wc.teams) == 0:
+                wc.teams.append(wp.detection.robots_yellow)
+                wc.teams.append(wp.detection.robots_blue)
+            else:
+                wc.teams[0] = (wp.detection.robots_yellow)
+                wc.teams[1] = (wp.detection.robots_blue)
 
             for i in wp.detection.balls:
               #Assuming that there is only one ball.
@@ -93,6 +106,28 @@ class RecvVision(threading.Thread):
             pass
           debugP ("************")
 
+def computeDistance(x1, y1, x2, y2):
+    xdis = x1 - x2
+    ydis = y1 - y2
+    distance = math.sqrt(xdis * xdis + ydis * ydis)
+
+    return distance
+
+def rotatePoint(x1, y1, x2, y2, angle):
+    s = math.sin(angle)
+    c = math.cos(angle)
+
+    x1 -= x2
+    y1 -= y2
+
+    newX = x1 * c - y1 * s
+    newY = x1 * s + y1 * c
+
+    x1 = newX + x2
+    y1 = newY + y2
+
+    return (x1, y1)
+
 class AposAI(threading.Thread):
     def __init__(self, name):
         threading.Thread.__init__(self)
@@ -104,26 +139,155 @@ class AposAI(threading.Thread):
         print ("Done with " + self.name)
 
     def computeAI(self):
+        global udpsocket
+        global shoot
+
+        packet = grSim_Packet()
+        packet.commands.isteamyellow = True
+        packet.commands.timestamp = 0.0
+        command = packet.commands.robot_commands.add()
+        command.id = 0
+
+        command.wheelsspeed = False
+        command.wheel1 = 0
+        command.wheel2 = 0
+        command.wheel3 = 0
+        command.wheel4 = 0
+        command.veltangent = 0 #positive -> Go forward
+        command.velnormal = 0 #positive -> Go left side
+        command.velangular = 0 #Rotate by angle
+
+        command.kickspeedz = 0
+        command.spinner = False
+
+        while playAll and len(wc.teams) == 0:
+            pass
+
         while playAll:
-            if len(wc.teams) > 0:
-                print ("Ball at:")
-                print ("\tx: {}".format(wc.ball.x))
-                print ("\ty: {}".format(wc.ball.y))
-                print ("Robot 0 of {}:".format(len(wc.teams[0])))
-                print ("\tx: {}".format(wc.teams[0][0].x))
-                print ("\ty: {}".format(wc.teams[0][0].y))
-                print ("\tOri: {}".format(wc.teams[0][0].orientation))
+            goalX = -3133
+            goalY = -69
 
-                print ("************")
+            if not shoot:
+                command.kickspeedx = 0
+                bX = wc.ball.x
+                bY = wc.ball.y
+            else:
+                command.kickspeedx = 10
+                bX = goalX
+                bY = goalY
+                shoot = False
 
-                pass
+            pX = wc.teams[0][0].x
+            pY = wc.teams[0][0].y
+
+            angle = math.atan2((bY - pY), (bX - pX))
+            if fakeOri == 0:
+                aimAngle = - wc.teams[0][0].orientation
+                angle = angle
+            elif fakeOri == 1:
+                aimAngle = angle
+                angle = math.pi * 2
+            elif fakeOri == 2:
+                aimAngle = angle + math.pi
+                angle = math.pi + math.pi / 2
+            elif fakeOri == 3:
+                aimAngle = angle
+                angle = math.pi
+            elif fakeOri == 4:
+                aimAngle = angle + math.pi
+                angle = math.pi / 2
+            elif fakeOri == 5:
+                aimAngle = wc.teams[0][0].orientation + math.pi
+                angle = angle
+            elif fakeOri == 6:
+                aimAngle = wc.teams[0][0].orientation + math.pi * 2
+                angle = angle
+            elif fakeOri == 7:
+                aimAngle = - wc.teams[0][0].orientation + math.pi / 2
+                angle = angle
+            elif fakeOri == 8:
+                aimAngle = - wc.teams[0][0].orientation - math.pi / 2
+                angle = angle
+
+            bX, bY = rotatePoint(bX, bY, pX, pY, aimAngle)
+
+            #angle = The angle to look towards
+            #pX, pY = rotatePoint(pX, pY, bX, bY, -angle)
+
+            tempD = computeDistance(bX, bY, pX, pY)
+
+            if (bX == pX):
+                bX += 1
+            if (bY == pY):
+                bY += 1
+
+            ratioX = tempD / (bX - pX)
+            ratioY = tempD / (bY - pY)
+
+            #offsetX = bX - (bX - tempD)
+            #offsetX = bY - (bY - tempD)
+
+            command.velnormal = 1 / ratioY
+            command.veltangent = 1 / ratioX
+
+
+            #angle = 0
+
+            angleDiff = ((math.pi + (angle - wc.teams[0][0].orientation)) % (math.pi * 2)) - math.pi
+
+            command.velangular = angleDiff * 10
+
+            print ("Mode: {}".format(fakeOri))
+            print ("Angle: {}".format(angle))
+            print ("Diff: {}".format(angleDiff))
+            print ("RatioX: {}".format(ratioX))
+            print ("RatioY: {}".format(ratioY))
+
+            print ("Ball at:")
+            print ("\tx: {}".format(wc.ball.x))
+            print ("\ty: {}".format(wc.ball.y))
+            print ("Robot 0 of {}:".format(len(wc.teams[0])))
+            print ("\tx: {}".format(wc.teams[0][0].x))
+            print ("\ty: {}".format(wc.teams[0][0].y))
+            print ("\tOri: {}".format(wc.teams[0][0].orientation))
+
+            print ("************")
+
+            udpsocket.sendto(packet.SerializeToString(), (SEND_ADDR, SEND_PORT))
+
+            time.sleep(0.01)
+
+            pass
 
 thread1 = RecvVision("recv")
 thread2 = AposAI("send")
 thread1.start()
 thread2.start()
 
-input()
+txtInput = ""
+while txtInput is not "q":
+    txtInput = input()
+    if txtInput is "s":
+        shoot = True
+    elif txtInput is "0":
+        fakeOri = 0
+    elif txtInput is "1":
+        fakeOri = 1
+    elif txtInput is "2":
+        fakeOri = 2
+    elif txtInput is "3":
+        fakeOri = 3
+    elif txtInput is "4":
+        fakeOri = 4
+    elif txtInput is "5":
+        fakeOri = 5
+    elif txtInput is "6":
+        fakeOri = 6
+    elif txtInput is "7":
+        fakeOri = 7
+    elif txtInput is "8":
+        fakeOri = 8
+
 playAll = False
 
 print ("I had a good life. This is it though.")
